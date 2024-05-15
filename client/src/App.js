df2808384b516448af3300a9bed8a3ac75cce39a
Diego Cootra <diego.fonseca@cootravale.com.br>
2024-05-15 17:16:38 -0300

    nova home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import IndexPage from './pages/IndexPage';
+import HomePage from './pages/HomePage'; // Importe o HomePage
 import CadastroPage from './pages/CadastroPage';
 import LoginPage from './pages/LoginPage';
 
@@ -9,7 +9,8 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={<LoginPage />} />
+                <Route exact path="/" element={<HomePage />} /> {/* Altere a rota inicial para HomePage */}
+                <Route path="/login" element={<LoginPage />} />
                 <Route path="/cadastro" element={<CadastroPage />} />
             </Routes>
         </Router>
